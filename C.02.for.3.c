c29447098b75dd5509c938d06c930300bd290cc2
Mahir Sadman <mahirsadman321@gmail.com>
2025-11-02 15:40:05 +0600

    Create C.02.for.3.c

---
+++ C.02.for.3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int n;
+
+    printf("Enter the number:");
+    scanf("%d", &n);
+
+    for(int i=0; i <= n;i++)
+    {
+        printf(" %d", i);
+
+    }
+
+    return 0;
+}
